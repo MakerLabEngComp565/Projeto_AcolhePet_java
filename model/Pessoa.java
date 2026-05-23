0945cc028880c9cfbdbd70b5bb5cbe59a2564cfd
MakerLabEngComp565 <oceanoantarticoagua@gmail.com>
2026-05-23 13:59:01 -0300

    Add files via upload

---
+++ model/Pessoa.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String email;
+
+    public Pessoa(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + nome + " | Email: " + email;
+    }
+}
